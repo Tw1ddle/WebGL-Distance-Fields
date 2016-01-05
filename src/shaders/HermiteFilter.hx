35732ceff4ccbfe10ef227fe63dd8e80c45a1786
sam <sam@silver42.karoo.co.uk>
2016-01-05 23:12:57 +0000

    Bicubic hermite texture filtering shader

---
+++ src/shaders/HermiteFilter.hx
@@ -0,0 +1,14 @@
+package shaders;
+
+import three.Vector2;
+import util.FileReader;
+
+class HermiteFilter {	
+	public static var uniforms = {
+		tDiffuse: { type: "t", value: null },
+		texw: { type: "f", value: 0.0 },
+		texh: { type: "f", value: 0.0 }
+	};
+	public static var vertexShader = FileReader.readFile("shaders/passthrough.vertex");
+	public static var fragmentShader = FileReader.readFile("shaders/bicubichermite.fragment");
+}
